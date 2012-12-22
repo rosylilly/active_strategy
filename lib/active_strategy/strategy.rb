cd57e024018df88078c47383a9b963e674acc218
Sho Kusano <rosylilly@aduca.org>
2012-12-22 15:40:35 +0900

    Implement ActiveStrategy::Strategy

--- lib/active_strategy/strategy.rb
+++ lib/active_strategy/strategy.rb
@@ -1,4 +1,9 @@
 module ActiveStrategy
   module Strategy
+    def initialize(context = {})
+      context.each_pair do |key, val|
+        instance_variable_set(:"@#{key}", val)
+      end
+    end
   end
 end
--- spec/lib/active_strategy/strategy_spec.rb
+++ spec/lib/active_strategy/strategy_spec.rb
@@ -1,4 +1,19 @@
 require 'spec_helper'
 
 describe ActiveStrategy::Strategy do
+  let(:dummy_strategy) do
+    Class.new do
+      include ActiveStrategy::Strategy
+
+      attr_reader :dummy
+    end
+  end
+
+  describe '.new' do
+    subject(:strategy) { dummy_strategy.new(:dummy => true) }
+
+    it 'assigned dummy' do
+      strategy.dummy.should be_true
+    end
+  end
 end
