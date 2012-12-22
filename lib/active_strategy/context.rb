1514a980e93a596862eb97ea099791300e9c8d5d
Sho Kusano <rosylilly@aduca.org>
2012-12-22 15:37:17 +0900

    Implement ActiveStrategy::Context

--- lib/active_strategy/context.rb
+++ lib/active_strategy/context.rb
@@ -1,4 +1,31 @@
+require 'active_support/concern'
+
 module ActiveStrategy
   module Context
+    extend ActiveSupport::Concern
+
+    def self.define_stratgy_method(context, action, strategy)
+      context.instance_eval do
+        define_method(action) do |*args|
+          strategy.new({ self.class.name.downcase => self }).
+            send(action, *args)
+        end
+      end
+    end
+
+    module ClassMethods
+      def strategy(action, options = {})
+        options = options.reverse_merge(
+          :class_method => false
+        )
+
+        strategy = "#{action}_strategy".classify.constantize
+
+        context = self
+        context = (class << self; self; end) if options[:class_method]
+
+        Context.define_stratgy_method(context, action, strategy)
+      end
+    end
   end
 end
--- spec/lib/active_strategy/context_spec.rb
+++ spec/lib/active_strategy/context_spec.rb
@@ -1,4 +1,58 @@
 require 'spec_helper'
 
 describe ActiveStrategy::Context do
+  subject { User }
+
+  it { should respond_to(:strategy) }
+
+  describe '.strategy' do
+    class SampleStrategy
+      include ActiveStrategy::Strategy
+
+      attr_reader :class, :user
+
+      def sample
+        self
+      end
+    end
+
+    context 'when instance method' do
+      before do
+        User.strategy(:sample)
+      end
+
+      it('User#sample exists') do
+        User.instance_methods.should include(:sample)
+      end
+
+      it 'called SampleStrategy#sample' do
+        SampleStrategy.any_instance.should_receive(:sample)
+        User.new.sample
+      end
+
+      it 'assigned uer' do
+        user = User.new
+        user.sample.user.should == user
+      end
+    end
+
+    context 'when class method' do
+      before do
+        User.strategy(:sample, class_method: true)
+      end
+
+      it('User.sample exists') do
+        User.methods.should include(:sample)
+      end
+
+      it 'called SampleStrategy#sample' do
+        SampleStrategy.any_instance.should_receive(:sample)
+        User.sample
+      end
+
+      it 'assigned class' do
+        User.sample.class.should == User
+      end
+    end
+  end
 end
